8ee770146cf2c3d8b118f01168948c3c12cd428f
daibang2014 <nguyenquochoang57@gmail.com>
2024-07-06 02:01:43 +0700

    add crud account

--- src/main/java/dao/AccountDAO.java
+++ src/main/java/dao/AccountDAO.java
@@ -175,32 +175,6 @@ public class AccountDAO {
         return false;
     }
 
-    public boolean addRenter(int accountId, int roomId) {
-        String QUERY = "INSERT INTO [dbo].[renter]([account_id]\n" +
-                "           ,[motel_room_id]) VALUES (?,?)";
-        try (Connection conn = DBcontext.getConnection(); PreparedStatement pst = conn.prepareStatement(QUERY)) {
-            pst.setInt(1, accountId);
-            pst.setInt(2, roomId);
-            return pst.executeUpdate() > 0;
-        } catch (Exception e) {
-            e.printStackTrace();
-            return false;
-        }
-    }
-
-    public boolean deleteAccount(int accountId) {
-        String QUERY = "DELETE FROM [dbo].[accounts]\n" +
-                "      WHERE account_id = ?";
-        try (Connection conn = DBcontext.getConnection(); PreparedStatement pst = conn.prepareStatement(QUERY)) {
-            pst.setInt(1, accountId);
-
-            return pst.executeUpdate() > 0;
-        } catch (Exception e) {
-            e.printStackTrace();
-            return false;
-        }
-    }
-
     public List<Feedback> getFeedbacksForAdmin() {
         List<Feedback> feedbacks = new ArrayList<>();
         // Cập nhật câu truy vấn để lấy feedback với tag là 'Admin'
@@ -226,8 +200,138 @@ public class AccountDAO {
         return feedbacks;
     }
 
+    public List<Account> getAllAccount() {
+    List<Account> accounts = new ArrayList<>();
+    String sql = "SELECT * FROM accounts WHERE role != 'admin'";
+    try (Connection conn = DBcontext.getConnection();
+         PreparedStatement ps = conn.prepareStatement(sql)) {
+        ResultSet rs = ps.executeQuery();
+        while (rs.next()) {
+            Account account = new Account();
+            account.setAccountId(rs.getInt("account_id"));
+            account.setRole(rs.getString("role"));
+            account.setPhone(rs.getString("phone"));
+            account.setGender(rs.getBoolean("gender"));
+            account.setFullname(rs.getString("fullname"));
+            account.setEmail(rs.getString("email"));
+            account.setDob(rs.getDate("dob"));
+            account.setCreateDate(rs.getDate("create_date"));
+            account.setCitizenId(rs.getString("citizen_id"));
+            account.setAvatar(rs.getString("avatar"));
+            account.setActive(rs.getBoolean("active"));
+            accounts.add(account);
+        }
+    } catch (SQLException e) {
+        e.printStackTrace();
+    }
+    return accounts;
+    }
+    public boolean updateAccountStatus(int accountId, boolean isActive) {
+        String sql = "UPDATE accounts SET active = ? WHERE account_id = ?";
+        try (Connection conn = DBcontext.getConnection();
+             PreparedStatement ps = conn.prepareStatement(sql)) {
+            ps.setBoolean(1, isActive);
+            ps.setInt(2, accountId);
+            return ps.executeUpdate() > 0;
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+    public Account getAccountById(int accountId){
+        String sql = "SELECT * FROM accounts WHERE account_id = ?";
+        try (Connection conn = DBcontext.getConnection();
+             PreparedStatement ps = conn.prepareStatement(sql)) {
+            ps.setInt(1, accountId);
+            ResultSet rs = ps.executeQuery();
+            if (rs.next()) {
+                Account account = new Account();
+                account.setAccountId(rs.getInt("account_id"));
+                account.setRole(rs.getString("role"));
+                account.setPhone(rs.getString("phone"));
+                account.setGender(rs.getBoolean("gender"));
+                account.setFullname(rs.getString("fullname"));
+                account.setEmail(rs.getString("email"));
+                account.setDob(rs.getDate("dob"));
+                account.setCitizenId(rs.getString("citizen_id"));
+                account.setActive(rs.getBoolean("active"));
+                account.setPassword(rs.getString("password"));
+                return account;
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
 
+    public boolean addAccount(Account account) {
+        String sql = "INSERT INTO accounts (role, phone, password,gender, citizen_id, dob, active,email,fullname) VALUES (?, ?, ?, ?, ?, ?,?,?,?)";
+        try (Connection conn = DBcontext.getConnection();
+             PreparedStatement ps = conn.prepareStatement(sql)) {
+            ps.setString(1, account.getRole());
+            ps.setString(2, account.getPhone());
+            String hashedPassword = BCrypt.hashpw(account.getPassword(), BCrypt.gensalt());
+            ps.setString(3, hashedPassword);
+            ps.setBoolean(4, account.isGender());
+            ps.setString(5, account.getCitizenId());
+            if(account.getDob() == null) {
+                ps.setDate(6, null);
+            } else
+            ps.setDate(6, new java.sql.Date(account.getDob().getTime()));
+            ps.setBoolean(7, account.isActive());
+            ps.setString(8, account.getEmail());
+            ps.setString(9, account.getFullname());
+            ps.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return false;
+        }
+        return true;
+    }
 
+    public boolean updateAccount(Account account) {
+    // Check if the password needs to be updated
+    boolean updatePassword = account.getPassword() != null && !account.getPassword().isEmpty();
+
+    // Construct the SQL query dynamically based on whether the password needs to be updated
+    String sql = "UPDATE accounts SET fullname = ?, gender = ?, phone = ?, citizen_id = ?, active = ?, email = ?, role= ?, dob = ?";
+    if (updatePassword) {
+        sql += ", password = ?";
+    }
+    sql += " WHERE account_id = ?";
+
+    try (Connection conn = DBcontext.getConnection();
+         PreparedStatement ps = conn.prepareStatement(sql)) {
+        ps.setString(1, account.getFullname());
+        ps.setBoolean(2, account.isGender());
+        ps.setString(3, account.getPhone());
+        ps.setString(4, account.getCitizenId());
+        ps.setBoolean(5, account.isActive());
+        ps.setString(6, account.getEmail());
+        ps.setString(7, account.getRole());
+        if(account.getDob() == null) {
+            ps.setNull(8, java.sql.Types.DATE);
+        } else {
+            ps.setDate(8, new java.sql.Date(account.getDob().getTime()));
+        }
+
+        int index = 9;
+
+        if (updatePassword) {
+            // Hash the password if it's being updated
+            String hashedPassword = BCrypt.hashpw(account.getPassword(), BCrypt.gensalt());
+            ps.setString(index++, hashedPassword);
+        }
+
+        ps.setInt(index, account.getAccountId());
+
+        int rowsAffected = ps.executeUpdate();
+        return rowsAffected > 0;
+    } catch (SQLException e) {
+        e.printStackTrace();
+        return false;
+        }
+    }
     public static void main(String[] args) {
 //        listUsers().forEach(p -> System.out.println(p));
 //        System.out.println(searchUser("hoangnq417@gmail.com"));
