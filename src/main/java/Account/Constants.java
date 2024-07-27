4b8d2b9e7a15725b0aba8402cc7643e4d1b9fff8
daibang2014 <nguyenquochoang57@gmail.com>
2024-07-27 21:44:23 +0700

    final

--- src/main/java/Account/Constants.java
+++ src/main/java/Account/Constants.java
@@ -8,7 +8,7 @@ package Account;
 public class Constants {
   public static String GOOGLE_CLIENT_ID = "641055812351-oek687om0tcvhgbmfutel5qjn1cdd9nl.apps.googleusercontent.com";
   public static String GOOGLE_CLIENT_SECRET = "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";
-  public static String GOOGLE_REDIRECT_URI = "http://localhost:9999/Project/login-google";
+  public static String GOOGLE_REDIRECT_URI = "https://motel-eac476bfc319.herokuapp.com/login-google";
   public static String GOOGLE_LINK_GET_TOKEN = "https://accounts.google.com/o/oauth2/token";
   public static String GOOGLE_LINK_GET_USER_INFO = "https://www.googleapis.com/oauth2/v1/userinfo?access_token=";
   public static String GOOGLE_GRANT_TYPE = "authorization_code";
